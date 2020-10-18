3cf9a93deb5a68153246292ce0f29c1ddee1afa3
Semyon Kirekov <kirekov@gmail.com>
2020-10-18 12:30:56 +0300

    Project init

---
+++ src/main/java/com/kirekov/specificationbuilderspringbootstarter/SpecificationBuilderSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.kirekov.specificationbuilderspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationBuilderSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationBuilderSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirekov/specificationbuilderspringbootstarter/SpecificationBuilderSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirekov.specificationbuilderspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationBuilderSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
