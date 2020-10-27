78071613631ccd7a8a53c0735bfc91c7a4eb4620
Semyon Kirekov <kirekov@gmail.com>
2020-10-27 22:26:13 +0300

    Added project skeleton

---
+++ src/main/java/com/kirekov/spec_builder/SpringDataSpecificationBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.kirekov.spec_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSpecificationBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSpecificationBuilderApplication.class, args);
+	}
+
+}
