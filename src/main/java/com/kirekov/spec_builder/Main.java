b7813d22da4034f00d17d7fc9ae466df5c53c570
Semyon Kirekov <kirekov@gmail.com>
2020-10-28 21:55:33 +0300

    [#5] - Added Main class

---
+++ src/main/java/com/kirekov/spec_builder/Main.java
@@ -0,0 +1,11 @@
+package com.kirekov.spec_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
