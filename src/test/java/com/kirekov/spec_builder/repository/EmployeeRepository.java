214843fb613b037c1def1520444d03ba4106333b
Semyon Kirekov <kirekov@gmail.com>
2020-10-31 22:30:13 +0300

    [#7] - Enhanced EmployeeRepository with 'JpaSpecificationExecutor'

--- src/test/java/com/kirekov/spec_builder/repository/EmployeeRepository.java
+++ src/test/java/com/kirekov/spec_builder/repository/EmployeeRepository.java
@@ -2,8 +2,9 @@ package com.kirekov.spec_builder.repository;
 
 import com.kirekov.spec_builder.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 }
