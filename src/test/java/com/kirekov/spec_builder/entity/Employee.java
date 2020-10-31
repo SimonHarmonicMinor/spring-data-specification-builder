c2bf37e64511edce9850a8ad4b120372bf8c41da
Semyon Kirekov <kirekov@gmail.com>
2020-10-31 22:06:05 +0300

    [#7] - Added test entity Employee and repository to acccess it

---
+++ src/test/java/com/kirekov/spec_builder/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.kirekov.spec_builder.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@Getter
+@Entity
+@Table
+public class Employee {
+    @Id
+    @GeneratedValue
+    private final Long id;
+
+    private final String name;
+}
